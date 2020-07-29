5d881ba5efb969dcbe71980fc3fdbb7b52872fb5
Puneet <birdipuneet@gmail.com>
2020-07-29 12:50:48 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+### Running Script
+
+// node solution.js
+
+### Application Assumptions
+
+1.  In order to evaluate loads, there must be a historical datastore of accepted transactions to compare against. This will be done with a writeStream to history.txt.
+
+2.  Each load is provided by line in a .txt file. In order to evaluate values it must be converted to JSON.
+
+    - After doing some research I chose to use a readStream and writeStream because of scalability: If the dataset gets much larger, the application would be limited by resources. If I were to load in transaction history, input and output to arrays before writing to a file I'd likely be limited by memory usage after a certain file size.
+    - Streaming data may lead to slower performance but won't run into a memory limit.
+
+3.  The history.txt datastore will be formatted exactly as input is provided, in lines. Storing as JSON would allow for easier evaluation but I'm doing this for file consistency.
+
+4.  Example output has 999 lines but input has 1000, this may be due to a duplicate being ignored?
+
+5.  No external libraries installed. Although MomentJS would've been great for evaluating time keys.
+
+### Naming
+
+1. The incoming load will be refered to as 'load', until the data has been converted to floats and dates. From there it will be refered to as 'transaction'.
+2. Existing historical transactions will be referred to as 'record'.
+
+### Submission Thoughts
+
+I wasn't able to get a correctly working script but I've identified some issues that may help me fix things:
+
+1. Line alignment was off from example output: this may be due to a duplicate entry not being ignored properly show to file comparison is showing more errors even if responses for each load is correct.
+
+2. I think at some point the asynchronicity of one of the functions may have caused responses being returned in the proper order:
+
+   For example:
+
+   MY OUPUT:
+   line 31: {"id":"27940","customer_id":"120","accepted":false}
+   line 32: {"id":"21336","customer_id":"477","accepted":true}
+   line 33: {"id":"7843","customer_id":"35","accepted":false}
+
+   EXAMPLE OUTPUT
+   line 31: {"id":"21336","customer_id":"477","accepted":true}
+   line 32: {"id":"27940","customer_id":"120","accepted":false}
+   line 33: {"id":"7843","customer_id":"35","accepted":false}
+
+Overall, I'm disappointed I couldn't get it to work but I learned a lot while completing this assignment about streaming and comparing dates.
---
+++ solution.js
@@ -0,0 +1,177 @@
+'use strict';
+const fs = require('fs');
+const readline = require('readline');
+
+//Function to convert the .txt input file into JSON objects by reading each line.
+const validate = async () => {
+  //Read the file one line at a time
+  const rl = readline.createInterface({
+    //pass in the input as a stream refering to the input text file
+    input: fs.createReadStream('./input.txt'),
+  });
+
+  //await each line of the stream and evaluate if it meets parameters, write response accordingly
+  for await (const line of rl) {
+    //convert the line to JSON
+    let transaction = JSON.parse(line);
+    //remove the symbols sign from the load_amount and parseFloat for evaluation
+    transaction = {
+      ...transaction,
+      load_amount: Number(transaction.load_amount.replace(/[^0-9.-]+/g, '')),
+      time: new Date(transaction.time),
+    };
+
+    //evaluate the transaction with the evaluate() function and get result
+    const result = await evaluate(transaction);
+
+    //if evaluate returns true, spread the response object and set accepted:true
+    if (result) {
+      writeTransaction(line);
+      writeResponse(result, transaction);
+    } else if (!result) {
+      //if evaluate() returns false, spread the response object and set accepted: false
+      writeResponse(result, transaction);
+    }
+  }
+};
+
+//This function will evaluate the load against the prescribed limits, using the checkHistory function when the datastore needs to be referenced
+const evaluate = async (transaction) => {
+  //if the transaction is greater than 5000, reject
+  if (transaction.load_amount > 5000) {
+    return false;
+    //if the transaction is less than 5000, return the response from withinLimits which will futher analyze the transaction against history
+  } else {
+    return await withinLimits(transaction);
+  }
+};
+
+//check to see if customer history will allow for this transaction to be completed
+const withinLimits = async (transaction) => {
+  //destructure the transaction and extract load amount
+  const { load_amount } = transaction;
+  //get the records for this customer, destructure to seperate daily and weekly
+  const { dailyRecord, weeklyRecord } = await checkHistory(transaction);
+
+  //calculate the amount already loaded in this day
+  const dailySum = dailyRecord.reduce(
+    (acc, record) => acc + record.load_amount,
+    0
+  );
+  //calculate the amount already loaded this week
+  const weeklySum = weeklyRecord.reduce(
+    (acc, record) => acc + record.load_amount,
+    0
+  );
+  //if daily + load attempt > 5000, reject
+  //if dailyRecord.length === 3 , reject
+  if (dailyRecord.length >= 3 || dailySum + load_amount > 5000) {
+    return false;
+  } else if (weeklySum + load_amount > 20000) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+//this function returns an array of all transactions completed by the requesting customer
+const checkHistory = async (transaction) => {
+  //initiate empty customerHistory array
+  const customerRecords = {
+    dailyRecord: [],
+    weeklyRecord: [],
+  };
+
+  if (!fs.existsSync('./history.txt')) {
+    return customerRecords;
+  }
+
+  //create readline function
+  const rl = readline.createInterface({
+    //pass in the input as a stream refering to the histoy text file
+    input: fs.createReadStream('./history.txt'),
+  });
+
+  //await each line of the stream and evaluate if it meets parameters, push to array or continue
+  for await (const line of rl) {
+    //convert the line to JSON
+    let record = JSON.parse(line);
+    //use regex to remove symbols to convert amount to number, and convert date string to date object
+    record = {
+      ...record,
+      load_amount: Number(record.load_amount.replace(/[^0-9.-]+/g, '')),
+      time: new Date(record.time),
+    };
+
+    //EVALUATE EACH RECORD
+    if (
+      record.customer_id === transaction.customer_id &&
+      sameDay(record.time, transaction.time)
+    ) {
+      customerRecords.dailyRecord.push(record);
+      // if the record is within the same week as the transaction, push to customerRecord.weeklyRecord
+    } else if (
+      record.customer_id === transaction.customer_id &&
+      sameWeek(record.time, transaction.time)
+    ) {
+      customerRecords.weeklyRecord.push(record);
+    } else {
+      continue;
+    }
+  }
+  return customerRecords;
+};
+
+//UTLITY FUNCTIONS ====================
+//This function writes the accepted transaction to ./history.txt
+const writeTransaction = (transaction) => {
+  const logger = fs.createWriteStream('./history.txt', {
+    flags: 'a', //This will append the file rather than re-writing the whole thing
+  });
+  //add '\n' to the transaction so that it's written in a new line
+  const record = transaction + '\n';
+  logger.write(record);
+};
+
+//write the response to output.txt
+const writeResponse = (accepted, transaction) => {
+  //build response object
+  let response = {
+    id: transaction.id,
+    customer_id: transaction.customer_id,
+    accepted: accepted,
+  };
+  const logger = fs.createWriteStream('./output.txt', {
+    flags: 'a', //This will append the file rather than re-writing the whole thing
+  });
+  //stringify and add '\n' to the response so that it's written in a new line
+  const record = JSON.stringify(response) + '\n';
+  logger.write(record);
+};
+
+//check if two dates are the same day
+const sameDay = (d1, d2) => {
+  return (
+    d1.getDate() === d2.getDate() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getFullYear() === d2.getFullYear()
+  );
+};
+
+//check if two dates are within the same week
+const sameWeek = (d1, d2) => {
+  return d1.getWeek() === d2.getWeek();
+};
+
+//THIS METHOD WAS PULLED
+//Create Date prototype method to return which ISO week of the year a date is in
+Date.prototype.getWeek = function () {
+  //get the year of the date passed in
+  var onejan = new Date(this.getFullYear(), 0, 1);
+  //get the current day
+  var today = new Date(this.getFullYear(), this.getMonth(), this.getDate());
+  var dayOfYear = (today - onejan + 86400000) / 86400000;
+  return Math.ceil(dayOfYear / 7);
+};
+
+validate();
